000b8a50692bcf5bb2e9dc47b59fd228f1e8a598
yuzihao.2001 <yuzihao.2001@n21-216-067.byted.org>
2021-11-29 18:20:04 +0800

    summit version

--- src/test_device.cpp
+++ src/test_device.cpp
@@ -1,4 +1,5 @@
 #include "packetio.h"
+#include "route.h"
 
 
 // check if all the devices are able to activare
@@ -6,69 +7,78 @@ bool device_activate_test() {
     char* errbuf = NULL;
     pcap_if_t * pcap_it;
     if(pcap_findalldevs(&pcap_it, errbuf) < 0) {
-        my_printf("findalldevs error: %s", errbuf);
+        printf("findalldevs error: %s", errbuf);
         return 0;
     }
     for (pcap_if_t* it = pcap_it; it; it = it ->next) {
         pcap_t* _pcap = pcap_create(it -> name, errbuf); 
         u_char * mac = GetMACAddr(it -> name);
         if(_pcap == NULL) {
-            my_printf("pcap create error %s\n", errbuf);
+            printf("pcap create error %s\n", errbuf);
             continue;
         }
         int err = pcap_activate(_pcap);
         if(err != 0) {
-            my_printf("pcap_activate error\n");
+            printf("pcap_activate error\n");
             continue;
         }
-        my_printf("%s activate successfully, mac address is %s\n", it->name, mac);
+        printf("%s activate successfully, mac address is %s\n", it->name, mac);
     }
     pcap_freealldevs(pcap_it);
     return 1;
 }
 
+// check methods of DeivcePool
 bool device_test() {
-    pool.addDevice("en0");
-    pool.addDevice("en1");
+    char* errbuf = NULL;
+    pcap_if_t * pcap_it;
+    if(pcap_findalldevs(&pcap_it, errbuf) < 0) {
+        printf("findalldevs error: %s", errbuf);
+        return 0;
+    }
+    for (pcap_if_t* it = pcap_it; it; it = it->next) {
+        if(pool.addDevice(it->name) < 0) {
+            continue;
+        }
+    }
     int n = pool.device_list.size();
     for (int i = 0; i < n; ++i) {
         struct Device* dev = pool.device_list[i];
-        // dev -> printDeviceInfo();
         int id = pool.findDevice(dev->name);
         if(dev->id != id) {
-            my_printf("findDevice %s error\n", dev->name);
+            printf("findDevice %s error\n", dev->name);
             return 0;
         }
     }
     return 1;
 }
 
-void sendframes(Device* dev1, Device* dev2) {
-    for(int i = 0; i < 1; ++i) {
-        if(dev1->sendFrame("hello, world!", 13, ETHERTYPE_VLAN, dev2->mac) < 0)
-            return ;
-    }
-}
+// broadcastRouteTable to test send and receive.
 bool send_receive_test() {
-    struct Device* dev1 = pool.device_list[0];
-    std::thread tmp(sendframes, dev1, dev1);
-    tmp.join();
-    dev1->t.join();
+    pool.StartListening();
+    int len = pool.device_list.size();
+    for (int i = 0 ; i < len; ++i) {
+        Device* dev = pool.device_list[i];
+        dev->broadcastRouteTable = std::thread(broadcastRouteTable, dev);
+    }
     return 1;
 }
 
 int main() {
     pool.setFrameReceiveCallback(myFrameReceivedCallback);
     if(!device_activate_test()) {
-        my_printf("device_activate_test failed!\n");
+        printf("device_activate_test failed!\n");
         return 0;
     }
     if(!device_test()) {
-        my_printf("device_activate_test failed!\n");
+        printf("device_activate_test failed!\n");
         return 0;
     }
     if(!send_receive_test()) {
-        my_printf("send_receive_test failed!\n");
+        printf("send_receive_test failed!\n");
+        return 0;
     }
+    printf("all tests pass!\n");
+    sleep(1000);
     return 0;
 }
